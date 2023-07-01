bc3fd6a8f059e325a88af317cd740e33df6dad7e
sourav <souravd2397@gmail.com>
2023-07-01 14:54:43 +0530

    Uploading project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,40 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './top/top.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { FourComponent } from './four/four.component';
+import { FiveComponent } from './five/five.component';
+import { SixComponent } from './six/six.component';
+import { SevenComponent } from './seven/seven.component';
+import { EaightComponent } from './eaight/eaight.component';
+import { NineComponent } from './nine/nine.component';
+import { TenComponent } from './ten/ten.component';
+import { ElevenComponent } from './eleven/eleven.component';
+import { TwelveComponent } from './twelve/twelve.component';
+import { ThirteenComponent } from './thirteen/thirteen.component';
+import { LastComponent } from './last/last.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent,
+    FourComponent,
+    FiveComponent,
+    SixComponent,
+    SevenComponent,
+    EaightComponent,
+    NineComponent,
+    TenComponent,
+    ElevenComponent,
+    TwelveComponent,
+    ThirteenComponent,
+    LastComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/thirteen/thirteen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thirteen',
+  templateUrl: './thirteen.component.html',
+  styleUrls: ['./thirteen.component.scss']
+})
+export class ThirteenComponent {
+
+}
